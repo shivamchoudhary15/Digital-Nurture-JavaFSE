d71f5058aca58ecffd7ec46769e0c90a8e4ad044
👋 Hi, I'm Shivam Choudhary <sc0626110@gmail.com>
2026-07-02 19:27:53 +0530

    Completed TDD using JUnit5 and Mockito exercises

---
+++ WEEK-1/TDD_using_JUnit5_and_Mockito/src/test/java/MockingAndStubbingTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MockingAndStubbingTest {
+
+    @Test
+    public void testExternalApi() {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        when(mockApi.getData()).thenReturn("Mock Data");
+        MyService service = new MyService(mockApi);
+        String result = service.fetchData();
+        assertEquals("Mock Data", result);
+    }
+}
